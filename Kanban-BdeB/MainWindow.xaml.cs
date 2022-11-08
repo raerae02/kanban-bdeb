959871938507ab73f81caee368a91fc17a5686d6
raed alkhatib <m.raed.khatib@gmail.com>
2022-11-08 16:54:34 -0500

    Commit Fonctionnalité du bouton À Propos

--- Kanban-BdeB/MainWindow.xaml.cs
+++ Kanban-BdeB/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace Kanban_BdeB
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Commandes pour les buttons
+
+        //Commande pour le menu À propos
+        public static RoutedCommand AProposCmd = new RoutedCommand();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Methodes pour le button À propos
+        private void APropos_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Kanban BdeB\n" +
+                "Version 0.1\n\n" +
+                "Auteur: Mohammad Raed Alkhatib"
+                );
+        }
+        private void APropos_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
         }
     }
 }
