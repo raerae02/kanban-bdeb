479f962eb0dd08fdcf2897df96736f1d1c24de17
raed alkhatib <m.raed.khatib@gmail.com>
2022-11-11 22:48:56 -0500

    commit creation des classes Etape et Tache

---
+++ Kanban-BdeB/Etape.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Kanban_BdeB
+{
+    public class Etape:IXMLSerializable
+    {
+        public bool EtapeTerminer
+        {
+            get;
+            set;
+        }
+        public int? NumeroEtape
+        {
+            get;
+            set;
+        }
+        public string DescriptionEtape
+        {
+            get;
+            set;
+        }
+
+        public Etape()
+        {
+            EtapeTerminer = false;
+            NumeroEtape = null;
+            DescriptionEtape = "";
+        }
+        
+        public Etape(XmlElement elementEtape)
+        {
+            FromXml(elementEtape);
+        }
+
+        public void FromXml(XmlElement xmlElement)
+        {
+            EtapeTerminer = Boolean.Parse(xmlElement.GetAttribute("termine"));
+            NumeroEtape = Int32.Parse(xmlElement.GetAttribute("no"));
+            DescriptionEtape = xmlElement.InnerText.Trim();
+
+        }
+
+        public XmlElement ToXml(XmlDocument xmlDocument)
+        {
+            XmlElement elementEtape = xmlDocument.CreateElement("etape");
+            elementEtape.SetAttribute("termine", EtapeTerminer.ToString());
+            elementEtape.SetAttribute("no", NumeroEtape.ToString());
+            elementEtape.InnerText = DescriptionEtape;
+
+            return elementEtape;
+        }
+    }
+}
---
+++ Kanban-BdeB/Tache.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Kanban_BdeB
+{
+    internal class Tache : IXMLSerializable
+    {
+        public DateOnly DateCreation
+        {
+            get;
+            set;
+        }
+        public DateOnly DateDebut
+        {
+            get;
+            set;
+        }
+        public DateOnly DateFin
+        {
+            get;
+            set;
+        }
+        public string Description
+        {
+            get;
+            set;
+        }
+        public ObservableCollection<Etape> Etapes
+        {
+            get;
+            set;
+        }
+
+        public Tache()
+        {
+            DateCreation = new DateOnly();
+            DateDebut = new DateOnly();
+            DateFin = new DateOnly();
+            Description = "";
+            Etapes = new ObservableCollection<Etape>();
+        }
+        public Tache(XmlElement xmlElement)
+        {
+            FromXml(xmlElement);
+        }
+
+        //Recuperation de l'element xml et classer ses données dans les attributs de la classe Tache
+
+        public void FromXml(XmlElement xmlElement)
+        {
+            DateCreation = DateOnly.Parse(xmlElement.GetAttribute("creation"));
+            DateDebut = DateOnly.Parse(xmlElement.GetAttribute("debut"));
+            DateFin = DateOnly.Parse(xmlElement.GetAttribute("fin"));
+
+            XmlElement description = xmlElement["description"];
+            Description = description.InnerText.Trim();
+
+            foreach(Etape etape in Etapes)
+            {
+                //IM NOT SURE ABOUT THIS
+                etape.FromXml(xmlElement);
+                Etapes.Add(etape);
+            }
+        }
+        //Recuperation du document xml et retourner un element xml representant un objet
+        public XmlElement ToXml(XmlDocument xmlDocument)
+        {
+            XmlElement elementTache = xmlDocument.CreateElement("tache");
+            elementTache.SetAttribute("creation", DateCreation.ToString());
+            elementTache.SetAttribute("debut", DateDebut.ToString());
+            elementTache.SetAttribute("fin", DateFin.ToString());
+
+            XmlElement elementDescription = xmlDocument.CreateElement("description");
+            elementDescription.SetAttribute("description", Description);
+            
+            return elementTache;
+        }
+    }
+}
