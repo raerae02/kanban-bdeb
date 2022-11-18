962a05d36c38702140beb6ea654f087a2b40aa05
raed alkhatib <m.raed.khatib@gmail.com>
2022-11-17 21:12:49 -0500

    commit commentaires tests et optimization des methodes

--- Tests/TestTache.cs
+++ Tests/TestTache.cs
@@ -6,73 +6,55 @@ using System.Text;
 
 namespace Tests
 {
+    /// <summary>
+    /// Testes pour la logique des taches
+    /// </summary>
     public class TestTache
     {
+        //Variable strings à tester
         private string? strTacheP;
         private string? strTacheEC;
         private string? strTacheT;
+
+        //Objet tache 
         private Tache? objTache;
 
         [SetUp]
         public void Setup()
         {
-            strTacheP = intialiserTachePl(new DateOnly(2022, 10, 31), null, null, "Préparation du TP #3 de 420-3GP-BB (A2022)",
-                false, 1, "Décider sujet TP");
+            strTacheP = intialiserTache(new DateOnly(2022, 10, 31), null, null, "Préparation du TP #3 de 420-3GP-BB (A2022)", false, false, 1, 2, "Faire la grille de correction", "Récupérer travaux sur GitHub");
 
-            strTacheEC = intialiserTacheEC(new DateOnly(2022, 10, 31), new DateOnly(2022, 11, 1), null,
-                "test2", true, false, 0, "test3", "test4");
+            strTacheEC = intialiserTache(new DateOnly(2022, 10, 24), new DateOnly(2022, 10, 24), null, "Correction du TP #1 de 420-3GP-BB (A2022)", true, false, 1 ,3 , "Faire la grille de correction", "Corriger 3 travau");
 
-            strTacheT = intialiserTacheT(new DateOnly(2022, 10, 31), new DateOnly(2022, 11, 1), new DateOnly(2022, 11, 2),
-                "test5", true, true, 0, "test6", "test7");
+            strTacheT = intialiserTache(new DateOnly(2022, 10, 28), new DateOnly(2022, 10, 28), new DateOnly(2022, 11, 15), "Préparation du TP #2 de 420-3GP-BB (A2022)", true, true, 1, 2, "Décider sujet TP", "Préparer données de travail");
 
         }
 
-        public string intialiserTacheT(DateOnly? dateCreation, DateOnly? dateDebut, DateOnly? dateFin,
-           string Description, bool eT1, bool eT2, int NumeroEtape, string descE1, string descE2)
+        /// <summary>
+        /// Methodes pour l'initialisation d'une tache.
+        /// Elle sert a retourner un string avec les attributs d'une tache qui sera tester dans les tests
+        /// </summary>
+        public string intialiserTache(DateOnly? dateCreation, DateOnly? dateDebut, DateOnly? dateFin, string Description, bool etapeTerminer1, bool etapeTerminer2, int NumeroEtape1, int NumeroEtape2, string descriptionEtape1, string descriptionEtape2)
         {
             string strTache = @$"<tache creation=""{dateCreation}"" debut=""{dateDebut}"" fin=""{dateFin}"">
                              <description>{Description}</description>
                              <etapes>
-                                <etape termine=""{eT1}"" no=""{NumeroEtape += 1}"">""{descE1}""</etape>
-                                <etape termine=""{eT2}"" no=""{NumeroEtape += 1}"">""{descE2}""</etape>
+                                <etape termine=""{etapeTerminer1}"" no=""{NumeroEtape1}"">""{descriptionEtape1}""</etape>
+                                <etape termine=""{etapeTerminer2}"" no=""{NumeroEtape2}"">""{descriptionEtape2}""</etape>
                              </etapes>
                           </tache>";
 
             return strTache;
         }
 
-        public string intialiserTacheEC(DateOnly? dateCreation, DateOnly? dateDebut, DateOnly? dateFin,
-           string Description, bool eT1, bool eT2, int NumeroEtape, string descE1, string descE2)
-        {
-            string strTache = @$"<tache creation=""{dateCreation}"" debut=""{dateDebut}"" fin=""{dateFin}"">
-                             <description>{Description}</description>
-                             <etapes>
-                                <etape termine=""{eT1}"" no=""{NumeroEtape += 1}"">""{descE1}""</etape>
-                                <etape termine=""{eT2}"" no=""{NumeroEtape += 1}"">""{descE2}""</etape>
-                             </etapes>
-                          </tache>";
-
-            return strTache;
-        }
-
-        public string intialiserTachePl(DateOnly? dateCreation, DateOnly? dateDebut, DateOnly? dateFin,
-            string Description, bool EtapeTerminer, int NumeroEtape, string DescriptionEtape)
-        {
-            string strTache = @$"<tache creation=""{dateCreation}"" debut=""{dateDebut}"" fin=""{dateFin}"">
-                             <description>{Description}</description>
-                             <etapes>
-                                <etape termine=""{EtapeTerminer}"" no=""{NumeroEtape}"">""{DescriptionEtape}""</etape>
-                             </etapes>
-                          </tache>";
-
-            return strTache;
-        }
-
-        [Test]
+        /// <summary>
+        /// Teste qui vérifie que lorsque toutes les étapes d’une tâche sont complétées, son statut est : Terminée.
+        /// </summary>
+                [Test]
         public void TestStatutTermine()
         {
             bool checkStatutTermine = false;
-            ChargerTacheTermineeXML();
+            ChargerTacheXML(strTacheT);
             int countDone = 0;
             if (objTache.DateCreation != null && objTache.DateDebut != null && objTache.DateFin != null)
             {
@@ -93,12 +75,15 @@ namespace Tests
             Assert.AreEqual(true, checkStatutTermine);
         }
 
+        /// <summary>
+        /// Teste qui vérifie que lorsque la première étape est complétée, son statut est : En cours.
+        /// </summary>
         [Test]
         public void TestStatutEncours()
         {
             int countDone = 0;
             bool checkStatutEnCours = false;
-            ChargerTacheEnCoursXML();
+            ChargerTacheXML(strTacheEC);
             if (objTache.DateCreation != null && objTache.DateDebut != null && objTache.DateFin == null)
             {
                 foreach (Etape etape in objTache.Etapes)
@@ -118,11 +103,14 @@ namespace Tests
             Assert.AreEqual(true, checkStatutEnCours);
         }
 
+        /// <summary>
+        /// Teste qui vérifie que lorsqu'une tache est crée, son statut est : Planifié.
+        /// </summary>
         [Test]
         public void TestStatutPlanifie()
         {
             bool checkStatutPlanifiee = false;
-            ChargerTachePlanifieeXML();
+            ChargerTacheXML(strTacheP);
             int countDone = objTache.Etapes.Count;
 
             if (objTache.DateCreation != null && objTache.DateDebut == null && objTache.DateFin == null)
@@ -144,25 +132,10 @@ namespace Tests
             Assert.AreEqual(true, checkStatutPlanifiee);
 
         }
-
-        void ChargerTacheTermineeXML()
+        void ChargerTacheXML(string strTache)
         {
             XmlDocument doc = new XmlDocument();
-            doc.LoadXml(strTacheT);
-            objTache = new Tache(doc.DocumentElement);
-        }
-
-        void ChargerTacheEnCoursXML()
-        {
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(strTacheEC);
-            objTache = new Tache(doc.DocumentElement);
-        }
-
-        void ChargerTachePlanifieeXML()
-        {
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(strTacheP);
+            doc.LoadXml(strTache);
             objTache = new Tache(doc.DocumentElement);
         }
     }
